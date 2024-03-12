5dd11f8fdab190f8db8cc18e247df421fa78f816
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-12 18:37:42 -0400

    only exit when q is pressed

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,21 @@
-use std::{
-    thread,
-    time,
-    io::{Stdout, Write},
-};
+use std::io::{Stdout, Write};
+use crossterm::event::{read, Event, KeyCode};
+
+const EXIT_EVENT: KeyCode = KeyCode::Char('q');
 
 pub fn run_editor(output: &mut Stdout) -> std::io::Result<()> {
-    thread::sleep(time::Duration::from_secs(2));
     write!(output, "Hey\r\n")?;
+    loop {
+        let input = read()?;
+        match input {
+            Event::Key(key) => match key.code {
+                EXIT_EVENT => break,
+                _ => ()
+            },
+            _ => ()
+        }
+    }
     write!(output, "Bye\r\n")?;
+
     Ok(())
 }
