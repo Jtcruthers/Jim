30c66b0cfe1f58ed74e7876b93b9a3aa4f51f0b1
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-12 18:17:00 -0400

    enable and disable raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
 use std::{
     fs,
+    io::{stdout, Stdout, Write},
     thread,
     time,
-    io::stdout
 };
 use clap::Parser;
-use crossterm::{
-    execute,
-    terminal::{Clear, ClearType, SetSize, ScrollUp, size},
-};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
 
 #[derive(Parser, Debug)]
 #[command(author = "Justin Carruthers", about = "Justin Vim")]
@@ -17,32 +14,28 @@ struct Args {
     filename: String
 }
 
-fn show_editor() -> std::io::Result<()> {
-    execute!(
-        stdout(),
-        Clear(ClearType::All),
-        SetSize(10, 10),
-        ScrollUp(5)
-    )?;
+fn run_editor(output: &mut Stdout) -> std::io::Result<()> {
 
-    thread::sleep(time::Duration::from_secs(5));
-
+    thread::sleep(time::Duration::from_secs(2));
+    write!(output, "Hey\r\n")?;
+    write!(output, "Bye\r\n")?;
     Ok(())
 }
 
 
 fn main() -> std::io::Result<()> {
     let args = Args::parse();
-    let (cols, rows) = size()?;
-
     let _ = fs::read(&args.filename).expect("Cant read file");
 
-    show_editor()?;
+    let mut stdout = stdout();
 
-    execute!(
-        stdout(),
-        SetSize(cols, rows)
-    )?;
+    enable_raw_mode()?;
 
+    match run_editor(&mut stdout) {
+        Result::Err(_) => disable_raw_mode()?,
+        _ => ()
+    }
+
+    disable_raw_mode()?;
     Ok(())
 }
