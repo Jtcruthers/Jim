949e0a1d7be0ca3d563b304d8f6bbd6b353f2c4f
Justin Carruthers <Jtcruthers@gmail.com>
2024-03-12 17:39:53 -0400

    added crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::{fs, io::stdout};
 use clap::Parser;
+use crossterm::{
+    execute,
+    style::{Color, Print, SetForegroundColor},
+    terminal::{Clear, ClearType},
+};
 
 #[derive(Parser, Debug)]
 #[command(author = "Justin Carruthers", about = "Justin Vim")]
@@ -7,8 +13,16 @@ struct Args {
     filename: String
 }
 
-fn main() {
+fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
-    ()
+    let _ = fs::read(&args.filename).expect("Cant read file");
+    execute!(
+        stdout(),
+        Clear(ClearType::All),
+        SetForegroundColor(Color::Red),
+        Print("Got the file\n"),
+    )?;
+
+    Ok(())
 }
